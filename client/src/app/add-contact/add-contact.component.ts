2ae02335bc1ecbd2194567de8d1c85bc9481d9b1
mdazharuddin1011999 <mdazharuddin1011999@gmail.com>
2021-03-13 12:22:22 +0530

    completed add-contact component

--- client/src/app/add-contact/add-contact.component.ts
+++ client/src/app/add-contact/add-contact.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpRequest, HttpHeaders } from '@angular/common/http';
+import { Contact } from '../models/contact';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-add-contact',
@@ -6,10 +9,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-contact.component.scss']
 })
 export class AddContactComponent implements OnInit {
+  loading: boolean = false;
+  newContact: Contact|null = null;
 
-  constructor() { }
+  constructor(public http: HttpClient) { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(form: NgForm){
+    this.loading = true;
+    const formValues = Object.assign({}, form.value);
+    const contact: Contact = {
+      name: `${formValues.firstName} ${formValues.lastName}`,
+      address: formValues.address,
+      phone: `${formValues.areaCode} ${formValues.prefix}-${formValues.lineNumber}`,
+      photoUrl: formValues.photo,
+    };
+
+    const header = new HttpHeaders();
+    header.append('Content-Type', 'application/json');
+
+    this.http.post<Contact>('/api/contacts', contact, {headers: header})
+      .subscribe(data=>{
+        form.reset();
+        this.loading = false;
+        console.log(data);
+        this.newContact = data;
+      });
   }
 
 }
