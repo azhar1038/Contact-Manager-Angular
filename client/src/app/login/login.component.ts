a31875b1b2669e2e0cb771b7581f71f1e897094e
mdazharuddin1011999 <mdazharuddin1011999@gmail.com>
2021-03-13 18:13:11 +0530

    Implemented login logout

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ApiService } from '../services/api.service';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private api: ApiService,
+    private auth: AuthService,
+    private router: Router,
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(form: NgForm){
+    const values = form.value;
+
+    const payload = {
+      username: values.username,
+      password: values.password
+    };
+
+    this.api.post('authenticate', payload)
+      .subscribe(data=>{
+        this.auth.setToken(data.token);
+        this.router.navigate(['/contacts']);
+      });
   }
 
 }
--- client/src/app/services/api.service.ts
+++ client/src/app/services/api.service.ts
@@ -3,6 +3,7 @@ import { environment } from 'src/environments/environment';
 import { HttpClient, HttpHeaders, HttpErrorResponse} from '@angular/common/http';
 import { catchError } from 'rxjs/operators';
 import { ObservableInput, throwError } from 'rxjs';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
@@ -10,23 +11,30 @@ import { ObservableInput, throwError } from 'rxjs';
 export class ApiService {
 
   private baseUrl = environment.apiUrl;
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    private auth: AuthService,
+  ) { }
 
   get<T>(url: string){
     const uri: string = `${this.baseUrl}/${url}`;
 
-    const headers = new HttpHeaders();
-    headers.append("Content-Type", "application/json");
+    const headers: HttpHeaders = new HttpHeaders({
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${this.auth.getToken()}`
+    });
 
-    return this.http.get<T>(uri, {headers})
+    return this.http.get<T>(uri, {headers: headers})
       .pipe(catchError(this.handleError));
   }
 
   post<T>(url: string, body: Object){
     const uri: string = `${this.baseUrl}/${url}`;
 
-    const headers = new HttpHeaders();
-    headers.append("Content-Type", "application/json");
+    const headers: HttpHeaders = new HttpHeaders({
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${this.auth.getToken()}`
+    });
 
     return this.http.post<T>(uri, body, {headers})
       .pipe(catchError(this.handleError));
